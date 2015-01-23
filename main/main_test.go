0438400dae1d8888e6f4b00ed2a45a671b500b07
Simon Leung <leungs@us.ibm.com>
2015-01-22 17:45:24 -0800

    init main module
    [#86167280]
    
    Signed-off-by: Jonathan Berkhahn <jaberkha@us.ibm.com>

---
+++ main/main_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	. "github.com/cloudfoundry-incubator/cli-plugin-repo/main"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Main", func() {
+
+})
