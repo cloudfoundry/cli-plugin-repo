1bc0da1dbe047800b25a53c815b0155c5c423c9c
Simon Leung <contact@simonleung.info>
2015-07-09 11:17:24 -0700

    sort by last updated methods
    [#98060086]

--- models/plugins.go
+++ models/plugins.go
@@ -146,3 +146,15 @@ func optionalStringField(v interface{}) string {
 	}
 	return ""
 }
+
+func (p PluginsJson) Len() int {
+	return len(p.Plugins)
+}
+
+func (p PluginsJson) Less(i, j int) bool {
+	return p.Plugins[i].Updated.After(p.Plugins[j].Updated)
+}
+
+func (p PluginsJson) Swap(i, j int) {
+	p.Plugins[i], p.Plugins[j] = p.Plugins[j], p.Plugins[i]
+}
