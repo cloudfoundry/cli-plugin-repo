25ee02542e7ceb5366ff302a8e06ea36adb83e34
Chris Hendrix <chendrix@pivotal.io>
2016-04-25 10:34:25 -0700

    Fix routing
    [fixes #118278477]

--- routes.go
+++ routes.go
@@ -13,12 +13,8 @@ var Routes = rata.Routes([]rata.Route{
 	{Path: "/list", Method: "GET", Name: List},
 
 	//Deprecated URI
-	{Path: "/ui", Method: "GET", Name: UI},
+	{Path: "/ui/", Method: "GET", Name: UI},
 
 	{Path: "/", Method: "GET", Name: Index},
-	{Path: "/js/:file", Method: "GET", Name: Index},
-	{Path: "/css/:file", Method: "GET", Name: Index},
-	{Path: "/font/:file", Method: "GET", Name: Index},
-	{Path: "/images/:file", Method: "GET", Name: Index},
-	{Path: "/:file", Method: "GET", Name: Index},
+	{Path: "/:file/", Method: "GET", Name: Index},
 })
