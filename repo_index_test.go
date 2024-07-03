bb392fa381bd105200b46b6510d9324c3632bc30
Shwetha Gururaj <shwetha.gururaj@broadcom.com>
2024-07-03 14:44:22 -0400

    Bump go and ginkgo versions [main] (#479)
    * Bump go and ginkgo. Add workflow for app deploy.

--- repo_index_test.go
+++ repo_index_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/blang/semver"
 	"gopkg.in/yaml.v2"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
 )
--- vendor/github.com/onsi/gomega/matchers/be_false_matcher.go
+++ vendor/github.com/onsi/gomega/matchers/be_false_matcher.go
@@ -1,11 +1,15 @@
+// untested sections: 2
+
 package matchers
 
 import (
 	"fmt"
+
 	"github.com/onsi/gomega/format"
 )
 
 type BeFalseMatcher struct {
+	Reason string
 }
 
 func (matcher *BeFalseMatcher) Match(actual interface{}) (success bool, err error) {
@@ -17,9 +21,17 @@ func (matcher *BeFalseMatcher) Match(actual interface{}) (success bool, err erro
 }
 
 func (matcher *BeFalseMatcher) FailureMessage(actual interface{}) (message string) {
-	return format.Message(actual, "to be false")
+	if matcher.Reason == "" {
+		return format.Message(actual, "to be false")
+	} else {
+		return matcher.Reason
+	}
 }
 
 func (matcher *BeFalseMatcher) NegatedFailureMessage(actual interface{}) (message string) {
-	return format.Message(actual, "not to be false")
+	if matcher.Reason == "" {
+		return format.Message(actual, "not to be false")
+	} else {
+		return fmt.Sprintf(`Expected not false but got false\nNegation of "%s" failed`, matcher.Reason)
+	}
 }
